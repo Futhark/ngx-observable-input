3d082eab8d65d02cb2c7881ee488e3846f081213
Michał Wróblewski <michal@wroblewski.mobi>
2020-06-05 22:15:05 +0200

    Added defaultValue argument to decorator

--- test-app/src/app/test-component/test-component.component.ts
+++ test-app/src/app/test-component/test-component.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { Observable } from "rxjs";
 import { ObservableInput } from "ngx-observable-input";
+import { first, tap } from "rxjs/operators";
 
 @Component({
     selector: "app-test-component",
@@ -11,6 +12,8 @@ export class TestComponentComponent implements OnInit, OnChanges {
     public bazinga: number;
     // tslint:disable-next-line:no-input-rename
     @ObservableInput() @Input("bazinga") public bazinga$: Observable<number>;
+    // tslint:disable-next-line: no-input-rename
+    @ObservableInput("this is the value for a non initialized input") @Input("notInitialized") public notInitialized$: Observable<number>;
 
     constructor() { }
 
